cff459fcb7dda31cd7195e66e41513e620db01d6
Bitcoin Bill <bitcoinbill000@protonmail.com>
2020-01-07 03:08:41 +0000

    swap depth chart to use bitmex data

--- src/App.js
+++ src/App.js
@@ -44,93 +44,16 @@ class App extends Component {
 
         let chart = am4core.create("chartdiv", am4charts.XYChart);
 
-        // Add data
-        chart.dataSource.url = "https://poloniex.com/public?command=returnOrderBook&currencyPair=USDT_BTC&depth=50";
-        chart.dataSource.reloadFrequency = 3000;
-        chart.dataSource.adapter.add("parsedData", function(data) {
-
-          // Function to process (sort and calculate cummulative volume)
-          function processData(list, type, desc) {
-
-            // Convert to data points
-            for(let i = 0; i < list.length; i++) {
-              list[i] = {
-                value: Number(list[i][0]),
-                volume: Number(list[i][1]),
-              }
-            }
-
-            // Sort list just in case
-            list.sort(function(a, b) {
-              if (a.value > b.value) {
-                return 1;
-              }
-              else if (a.value < b.value) {
-                return -1;
-              }
-              else {
-                return 0;
-              }
-            });
-
-            // Calculate cummulative volume
-            if (desc) {
-              for(let i = list.length - 1; i >= 0; i--) {
-                if (i < (list.length - 1)) {
-                  list[i].totalvolume = list[i+1].totalvolume + list[i].volume;
-                }
-                else {
-                  list[i].totalvolume = list[i].volume;
-                }
-                const dp = {};
-                dp["value"] = list[i].value;
-                dp[type + "volume"] = list[i].volume;
-                dp[type + "totalvolume"] = list[i].totalvolume;
-                res.unshift(dp);
-              }
-            }
-            else {
-              for(var i = 0; i < list.length; i++) {
-                if (i > 0) {
-                  list[i].totalvolume = list[i-1].totalvolume + list[i].volume;
-                }
-                else {
-                  list[i].totalvolume = list[i].volume;
-                }
-                const dp = {};
-                dp["value"] = list[i].value;
-                dp[type + "volume"] = list[i].volume;
-                dp[type + "totalvolume"] = list[i].totalvolume;
-                res.push(dp);
-              }
-            }
-
-          }
-
-          // Init
-          var res = [];
-          processData(data.bids, "bids", true);
-          processData(data.asks, "asks", false);
-
-          return res;
-        });
-
-        // Set up precision for numbers
         chart.numberFormatter.numberFormat = "#,###.";
 
-        // Create axes
         const xAxis = chart.xAxes.push(new am4charts.CategoryAxis());
         xAxis.dataFields.category = "value";
         xAxis.renderer.grid.template.disabled = true;
         xAxis.renderer.minGridDistance = 200;
-        // xAxis.title.text = "Price (BTC/ETH)";
 
         const yAxis = chart.yAxes.push(new am4charts.ValueAxis());
-        // yAxis.title.text = "Volume";
-        yAxis.renderer.minGridDistance = 200;
-        yAxis.renderer.grid.template.disabled = true;
+        yAxis.renderer.minGridDistance = 100;
 
-        // Create series
         const series = chart.series.push(new am4charts.StepLineSeries());
         series.dataFields.categoryX = "value";
         series.dataFields.valueY = "bidstotalvolume";
@@ -138,7 +61,6 @@ class App extends Component {
         series.stroke = am4core.color("#45a75e");
         series.fill = am4core.color("#b0dcba");
         series.fillOpacity = 1;
-        // series.tooltipText = "Ask: [bold]{categoryX}[/]\nTotal volume: [bold]{valueY}[/]\nVolume: [bold]{bidsvolume}[/]"
 
         const series2 = chart.series.push(new am4charts.StepLineSeries());
         series2.dataFields.categoryX = "value";
@@ -147,17 +69,16 @@ class App extends Component {
         series2.stroke = am4core.color("#e05a3f");
         series2.fill = am4core.color("#f4b9ac");
         series2.fillOpacity = 1;
-        // series2.tooltipText = "Ask: [bold]{categoryX}[/]\nTotal volume: [bold]{valueY}[/]\nVolume: [bold]{asksvolume}[/]"
 
-        // Add cursor
         chart.cursor = new am4charts.XYCursor();
 
         this.chart = chart;
     }
 
     componentDidMount() {
-        this.createDepthChart();
         this.addClient();
+        // TODO - only create the depth chart after the order book is built
+        this.createDepthChart();
     }
 
     componentWillUnmount() {
@@ -178,14 +99,6 @@ class App extends Component {
                 const table = json['table'];
                 if(table === 'trade') {
                     if(action === 'insert') {
-                        // let trades = this.state.trades;
-                        // trades = [...this.state.trades, ...json.data];
-                        // json.data.forEach(trade => {
-                        //     trades.push(trade);
-                        // });
-                        // if(trades.length > MAX_TRADES) {
-                        //     trades = trades.slice(trades.length-MAX_TRADES, trades.length);
-                        // }
                         this.setTrades([...json.data.reverse(), ...this.state.trades].slice(0, MAX_TRADES));
                     }
                 } else if(table === 'orderBookL2_25') {
@@ -214,9 +127,29 @@ class App extends Component {
                     var order_book_array = Object.keys(ORDER_BOOK).map(function(key) {
                         return ORDER_BOOK[key];
                     });
-                    const buyOrders = _.sortBy(order_book_array.filter(ob => ob['side'] === 'Buy'), ['price']);
+                    const buyOrders = _.sortBy(order_book_array.filter(ob => ob['side'] === 'Buy'), ['price']).reverse();
                     const sellOrders = _.sortBy(order_book_array.filter(ob => ob['side'] === 'Sell'), ['price']);
-                    // TODO - use these arrays to generate depth chart
+                    let depthData = [];
+                    let buyTotal = 0;
+                    buyOrders.forEach(order => {
+                        buyTotal += order.size;
+                        depthData.push({
+                            value: order.price,
+                            bidsvolume: order.size,
+                            bidstotalvolume: buyTotal
+                        })
+                    });
+                    depthData = depthData.reverse();
+                    let sellTotal = 0;
+                    sellOrders.forEach(order => {
+                        sellTotal += order.size;
+                        depthData.push({
+                            value: order.price,
+                            asksvolume: order.size,
+                            askstotalvolume: sellTotal
+                        })
+                    });
+                    this.chart.data = depthData;
                 }
             };
             CLIENTS['public'] = client;
