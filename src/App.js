61434c53794834ac4bc368cca78d38347722e72d
Bitcoin Bill <bitcoinbill000@protonmail.com>
2020-01-07 02:29:09 +0000

    added depth chart from poloniex

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { w3cwebsocket as W3CWebSocket } from "websocket";
-import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
@@ -9,9 +8,6 @@ import Typography from '@material-ui/core/Typography';
 import _ from 'lodash';
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
-import am4themes_animated from "@amcharts/amcharts4/themes/animated";
-
-am4core.useTheme(am4themes_animated);
 
 const CLIENTS = {};
 const ORDER_BOOK = {};
@@ -44,13 +40,130 @@ class App extends Component {
         this.setState({ trades });
     }
 
-    // const [newApiKey, setNewApiKey] = useState("");
-    // const [keys, setKeys] = useState(['1', '2', '3']);
-    // const [trades, setTrades] = useState([]);
+    createDepthChart() {
+
+        let chart = am4core.create("chartdiv", am4charts.XYChart);
+
+        // Add data
+        chart.dataSource.url = "https://poloniex.com/public?command=returnOrderBook&currencyPair=USDT_BTC&depth=50";
+        chart.dataSource.reloadFrequency = 3000;
+        chart.dataSource.adapter.add("parsedData", function(data) {
+
+          // Function to process (sort and calculate cummulative volume)
+          function processData(list, type, desc) {
+
+            // Convert to data points
+            for(let i = 0; i < list.length; i++) {
+              list[i] = {
+                value: Number(list[i][0]),
+                volume: Number(list[i][1]),
+              }
+            }
+
+            // Sort list just in case
+            list.sort(function(a, b) {
+              if (a.value > b.value) {
+                return 1;
+              }
+              else if (a.value < b.value) {
+                return -1;
+              }
+              else {
+                return 0;
+              }
+            });
+
+            // Calculate cummulative volume
+            if (desc) {
+              for(let i = list.length - 1; i >= 0; i--) {
+                if (i < (list.length - 1)) {
+                  list[i].totalvolume = list[i+1].totalvolume + list[i].volume;
+                }
+                else {
+                  list[i].totalvolume = list[i].volume;
+                }
+                const dp = {};
+                dp["value"] = list[i].value;
+                dp[type + "volume"] = list[i].volume;
+                dp[type + "totalvolume"] = list[i].totalvolume;
+                res.unshift(dp);
+              }
+            }
+            else {
+              for(var i = 0; i < list.length; i++) {
+                if (i > 0) {
+                  list[i].totalvolume = list[i-1].totalvolume + list[i].volume;
+                }
+                else {
+                  list[i].totalvolume = list[i].volume;
+                }
+                const dp = {};
+                dp["value"] = list[i].value;
+                dp[type + "volume"] = list[i].volume;
+                dp[type + "totalvolume"] = list[i].totalvolume;
+                res.push(dp);
+              }
+            }
+
+          }
+
+          // Init
+          var res = [];
+          processData(data.bids, "bids", true);
+          processData(data.asks, "asks", false);
+
+          return res;
+        });
+
+        // Set up precision for numbers
+        chart.numberFormatter.numberFormat = "#,###.";
+
+        // Create axes
+        const xAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+        xAxis.dataFields.category = "value";
+        xAxis.renderer.grid.template.disabled = true;
+        xAxis.renderer.minGridDistance = 200;
+        // xAxis.title.text = "Price (BTC/ETH)";
+
+        const yAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        // yAxis.title.text = "Volume";
+        yAxis.renderer.minGridDistance = 200;
+        yAxis.renderer.grid.template.disabled = true;
+
+        // Create series
+        const series = chart.series.push(new am4charts.StepLineSeries());
+        series.dataFields.categoryX = "value";
+        series.dataFields.valueY = "bidstotalvolume";
+        series.strokeWidth = 2;
+        series.stroke = am4core.color("#45a75e");
+        series.fill = am4core.color("#b0dcba");
+        series.fillOpacity = 1;
+        // series.tooltipText = "Ask: [bold]{categoryX}[/]\nTotal volume: [bold]{valueY}[/]\nVolume: [bold]{bidsvolume}[/]"
+
+        const series2 = chart.series.push(new am4charts.StepLineSeries());
+        series2.dataFields.categoryX = "value";
+        series2.dataFields.valueY = "askstotalvolume";
+        series2.strokeWidth = 2;
+        series2.stroke = am4core.color("#e05a3f");
+        series2.fill = am4core.color("#f4b9ac");
+        series2.fillOpacity = 1;
+        // series2.tooltipText = "Ask: [bold]{categoryX}[/]\nTotal volume: [bold]{valueY}[/]\nVolume: [bold]{asksvolume}[/]"
+
+        // Add cursor
+        chart.cursor = new am4charts.XYCursor();
+
+        this.chart = chart;
+    }
 
     componentDidMount() {
-        am4core.create("chartdiv", am4charts.XYChart);
+        this.createDepthChart();
         this.addClient();
+    }
+
+    componentWillUnmount() {
+        if (this.chart) {
+            this.chart.dispose();
+        }
     }
 
     addClient(apiKey, apiSecret) {
@@ -121,16 +234,21 @@ class App extends Component {
     render() {
         return (
           <Container>
-              <Grid container spacing={2}>
-                 <Grid item xs={3}>
+              <Grid container spacing={4}>
+                 {/*<Grid item xs={3}>
                      {this.state.keys.map((key) => {
                         return <div key={key}>{key}</div>;
                      })}
                      <input type="text" value={this.state.newApiKey} onChange={this.handleChangeNewApiKey} />
                      <Button variant="contained" color="primary" onClick={this.addNewApiKey}>Add</Button>
-                 </Grid>
-                 <Grid item xs={6}>
-                    <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
+                 </Grid>*/}
+                 <Grid item xs={9}>
+                    <Card>
+                        <CardContent>
+                            <Typography color="textSecondary" gutterBottom><strong>XBT/USD:</strong> Depth Chart</Typography>
+                            <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
+                        </CardContent>
+                    </Card>
                  </Grid>
                  <Grid item xs={3}>
                     <Card>
